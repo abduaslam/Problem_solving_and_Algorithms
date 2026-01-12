de8ee5273ec6de84f1e0fe4dfd06e02d3484e509
abduaslam <abduaslam098@gmail.com>
2026-01-12 10:00:13 +0200

    length of shortest word

---
+++ Shortest Word.js
@@ -0,0 +1,16 @@
+// Simple, given a string of words, return the length of the shortest word(s).
+
+// String will never be empty and you do not need to account for different data types.
+function findShort(s) {
+  let words = s.split(' ');
+  let shortest = words[0].length;
+
+  for (let word of words) {
+    if (word.length < shortest) {
+      shortest = word.length;
+    }
+  }
+  return shortest;
+}
+findShort("bitcoin take over the world maybe who knows");
+findShort("turns out random test cases are easier");     
