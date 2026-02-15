bcb80cdce8cc90638480384fe993e740d7a21f1a
abduaslam <abduaslam098@gmail.com>
2026-02-15 15:11:32 +0200

    Calculate average

--- Calculate average.js
+++ Calculate average.js
@@ -1,10 +1,12 @@
 // Write a function which calculates the average of the numbers in a given array.
 // Note: Empty arrays should return 0.
-function findAverage(array) {
-  if (array.length === 0) return 0;
-
-  const sum = array.reduce((a, b) => a + b, 0);
-  return sum / array.length;
-}
-
-console.log(findAverage([23,45,67,89,75,12]))
+ function averageArry(array){
+  if(array.length===0){
+    return 0;
+  }
+  else{
+    let sum=array.reduce((acc,n)=>acc+n)
+    return sum/array.length
+  }
+ }
+ console.log(averageArry([12,34,56,23,18,45]))
