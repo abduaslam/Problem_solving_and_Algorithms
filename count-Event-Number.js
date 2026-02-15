796ddd8c3b39b5abe013fa1d344dd8f9ded730a2
abduaslam <abduaslam098@gmail.com>
2026-02-15 15:23:44 +0200

    count-Event-Number

--- count-Event-Number.js
+++ count-Event-Number.js
@@ -2,13 +2,13 @@
 // check if each number is divisible by 2, and keep a count of the even numbers. 
 // Finally, it should return the total count.
 function countEvenNumbers(n) {
-    let evenCount = 0;
-    for (let i = 1; i <= n; i++) {
-        if (i % 2 == 0) {
-            evenCount += 1; 
-        }
+  let countevent=0;
+  for(let i=0;i<n;i++){
+    if(i%2==0){
+        countevent +=1
     }
-    return evenCount;
+  }
+  return countevent
 }
 
-console.log(countEvenNumbers(10));
+console.log(countEvenNumbers(15));
