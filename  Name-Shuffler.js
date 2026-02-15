62499f382823c41665c404b86be5e3cd3830a145
abduaslam <abduaslam098@gmail.com>
2026-02-15 13:54:49 +0200

    Name-Shuffler

---  Name-Shuffler.js
+++  Name-Shuffler.js
@@ -2,7 +2,7 @@
 // Name Shuffler
 // Write a function that returns a string in which firstname is 
 // swapped with last name.
-function nameShuffler(str){
-return str.split("").reverse().join("")
+function swapp(str){
+   return str.split(' ').reverse().join(" ")
 }
-console.log(" abdu aslam")
+ console.log(swapp("abdalsalm yagoub"))
